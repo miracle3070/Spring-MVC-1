e3a26589dc15e3c99249cd5711335e396e3a8fa1
miracle3070 <miracle3070@naver.com>
2022-06-29 01:14:23 +0900

    Model 추가 - v3

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -13,6 +13,6 @@ public class MemberFormControllerV2 implements ControllerV2 {
 
     @Override
     public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        return new MyView("/WEB-INF/views/new-forms.jsp");
+        return new MyView("/WEB-INF/views/new-form.jsp");
     }
 }
